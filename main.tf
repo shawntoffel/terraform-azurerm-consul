d002637711b6224ece6bb560a98ff74e795e9225
Shawn Toffel <shawn.toffel+github@gmail.com>
2018-11-27 19:48:33 -0700

    change scale set name to server

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ data "template_file" "user_data_client" {
   template = "${file("${path.module}/custom-data-client.sh")}"
 
   vars {
-    scale_set_name = "${var.cluster_name}-client"
+    scale_set_name = "${var.cluster_name}-server"
     subscription_id = "${var.subscription_id}"
     tenant_id = "${var.tenant_id}"
     client_id = "${var.client_id}"
